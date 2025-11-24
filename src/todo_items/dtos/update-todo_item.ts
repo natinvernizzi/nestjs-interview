519d2b6df7ad546efa22a5360e12773ab58ab5e2
Natalia Invernizzi <natinvernizzi11@gmail.com>
2025-11-24 10:06:19 -0300

    Adding validations

--- src/todo_items/dtos/update-todo_item.ts
+++ src/todo_items/dtos/update-todo_item.ts
@@ -1,4 +1,21 @@
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  IsBoolean,
+  IsOptional,
+} from 'class-validator';
+
 export class UpdateTodoItemDto {
-  name: string;
-  completed: boolean;
+  @IsOptional()
+  @IsString({ message: 'Name must be a string' })
+  @IsNotEmpty({ message: 'Name is mandatory' })
+  @MinLength(3, { message: 'Name must be at least 3 characters long' })
+  @MaxLength(100, { message: 'Name must be less than 100 characters' })
+  name?: string;
+
+  @IsOptional()
+  @IsBoolean({ message: 'Completed must be a boolean' })
+  completed?: boolean;
 }
--- src/todo_lists/dtos/update-todo_list.ts
+++ src/todo_lists/dtos/update-todo_list.ts
@@ -1,3 +1,4 @@
-export class UpdateTodoListDto {
-  name: string;
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTodoListDto } from './create-todo_list';
+
+export class UpdateTodoListDto extends PartialType(CreateTodoListDto) {}
