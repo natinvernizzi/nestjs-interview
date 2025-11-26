3df9606c088e858a5485b700d773ace451b94a34
Natalia Invernizzi <natinvernizzi11@gmail.com>
2025-11-26 10:30:39 -0300

    local updates for testing the FE

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // Enable CORS for the React frontend
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 
